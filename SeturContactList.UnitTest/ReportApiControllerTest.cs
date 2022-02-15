f32f3b0fc28ad28536ab60229b9c405918ea4f33
mustafaalkan64 <mustafaalkan64@gmail.com>
2022-02-15 12:52:44 +0300

    Report Api Controller Test Modified

--- SeturContactList.UnitTest/ReportApiControllerTest.cs
+++ SeturContactList.UnitTest/ReportApiControllerTest.cs
@@ -24,8 +24,6 @@ namespace SeturContactList.UnitTest
 {
     public class ReportApiControllerTest
     {
-        private readonly Mock<IGenericRepository<Reports>> _mockReportRepository;
-        private readonly Mock<IGenericRepository<PersonContacts>> _mockPersonContactRepository;
         private readonly Mock<IService<Reports>> _mockReportsService;
         private readonly Mock<IService<ReportDetail>> _mockReportDetailService;
         private readonly Mock<IPublishEndpoint> _mockPublishEndpoint;
@@ -35,137 +33,135 @@ namespace SeturContactList.UnitTest
         private List<ReportDetail> reportDetails;
         private Reports newReport;
         private ReportDetail newReportDetail;
-        //public ReportApiControllerTest()
-        //{
-        //    var myProfile = new MapProfile();
-        //    var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
-        //    mapper = new Mapper(configuration);
+        private Guid newReportId1 = Guid.NewGuid();
+        private Guid newReportId2 = Guid.NewGuid();
+        public ReportApiControllerTest()
+        {
+            var myProfile = new MapProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            mapper = new Mapper(configuration);
 
-        //    _mockReportRepository = new Mock<IGenericRepository<Reports>>();
-        //    _mockPersonContactRepository = new Mock<IGenericRepository<PersonContacts>>();
-        //    _mockReportsService = new Mock<IService<Reports>>();
-        //    _mockReportDetailService = new Mock<IService<ReportDetail>>();
-        //    _mockPublishEndpoint = new Mock<IPublishEndpoint>();
-        //    _controller = new ReportController(_mockReportsService.Object, _mockReportDetailService.Object, _mockPublishEndpoint.Object, mapper);
-        //    reports = new List<Reports>() { new Reports()
-        //    {
-        //        Id = 1,
-        //        ReportStatus = Core.ReportStatusEnum.Preparing,
-        //        CreatedDate = DateTime.Now,
-        //        ReportDetail = new ReportDetail()
-        //        {
-        //            Lat = 38,
-        //            Long = 27,
-        //            RegisteredPersonCount = 2,
-        //            RegisteredPhoneCount = 3
-        //        },
+            _mockReportsService = new Mock<IService<Reports>>();
+            _mockReportDetailService = new Mock<IService<ReportDetail>>();
+            _mockPublishEndpoint = new Mock<IPublishEndpoint>();
+            _controller = new ReportController(_mockReportsService.Object, _mockReportDetailService.Object, _mockPublishEndpoint.Object, mapper);
+            reports = new List<Reports>() { new Reports()
+            {
+                Id = newReportId1,
+                ReportStatus = Core.ReportStatusEnum.Preparing,
+                CreatedDate = DateTime.Now,
+                ReportDetail = new ReportDetail()
+                {
+                    Lat = 38,
+                    Long = 27,
+                    RegisteredPersonCount = 2,
+                    RegisteredPhoneCount = 3
+                },
 
-        //    },
-        //    new Reports()
-        //    {
-        //        Id = 2,
-        //        ReportStatus = Core.ReportStatusEnum.Completed,
-        //        CreatedDate = DateTime.Now,
-        //        ReportDetail = new ReportDetail()
-        //        {
-        //            Lat = 35,
-        //            Long = 28,
-        //            RegisteredPersonCount = 2,
-        //            RegisteredPhoneCount = 3
-        //        },
+                },
+                new Reports()
+                {
+                    Id = newReportId2,
+                    ReportStatus = Core.ReportStatusEnum.Completed,
+                    CreatedDate = DateTime.Now,
+                    ReportDetail = new ReportDetail()
+                    {
+                        Lat = 35,
+                        Long = 28,
+                        RegisteredPersonCount = 2,
+                        RegisteredPhoneCount = 3
+                    },
 
-        //    }
+                }
 
-        //    };
+            };
 
 
-        //    reportDetails = new List<ReportDetail>() { new ReportDetail()
+            reportDetails = new List<ReportDetail>() { new ReportDetail()
 
-        //       {
-        //            Lat = 38,
-        //            Long = 27,
-        //            RegisteredPersonCount = 2,
-        //            RegisteredPhoneCount = 3,
-        //            ReportId = 1,
-        //            CreatedDate = DateTime.Now
-        //        },
-        //        new ReportDetail()
-        //        {
-        //            Lat = 35,
-        //            Long = 28,
-        //            RegisteredPersonCount = 3,
-        //            RegisteredPhoneCount = 5,
-        //            ReportId = 2,
-        //            CreatedDate = DateTime.Now
-        //        }
+               {
+                    Id = Guid.NewGuid(),
+                    Lat = 38,
+                    Long = 27,
+                    RegisteredPersonCount = 2,
+                    RegisteredPhoneCount = 3,
+                    ReportId = newReportId1,
+                    CreatedDate = DateTime.Now
+                },
+                new ReportDetail()
+                {
+                    Id = Guid.NewGuid(),
+                    Lat = 35,
+                    Long = 28,
+                    RegisteredPersonCount = 3,
+                    RegisteredPhoneCount = 5,
+                    ReportId = newReportId2,
+                    CreatedDate = DateTime.Now
+                }
 
-        //    };
+            };
 
-        //    newReport = new Reports()
-        //    {
-        //        Id = 3,
-        //        ReportStatus = Core.ReportStatusEnum.Preparing,
-        //        CreatedDate = DateTime.Now,
-        //        ReportDetail = new ReportDetail()
-        //        {
-        //            Lat = 35,
-        //            Long = 28,
-        //            RegisteredPersonCount = 2,
-        //            RegisteredPhoneCount = 3
-        //        },
-        //    };
-        //}
+            newReport = new Reports()
+            {
+                Id = Guid.NewGuid(),
+                ReportStatus = Core.ReportStatusEnum.Preparing,
+                CreatedDate = DateTime.Now,
+                ReportDetail = new ReportDetail()
+                {
+                    Lat = 35,
+                    Long = 28,
+                    RegisteredPersonCount = 2,
+                    RegisteredPhoneCount = 3
+                },
+            };
+        }
 
-        //[Fact]
-        //public async void GetPerson_ActionExecutes_ReturnOkResultWithPerson()
-        //{
-        //    _mockReportsService.Setup(x => x.GetAllAsync()).ReturnsAsync(reports);
+        [Fact]
+        public async void GetReports_ActionExecutes_ReturnOkResultWithReports()
+        {
+            _mockReportsService.Setup(x => x.GetAllAsync()).ReturnsAsync(reports);
 
-        //    var result = await _controller.All();
+            var result = await _controller.All();
 
-        //    var okResult = Assert.IsType<ObjectResult>(result);
+            var okResult = Assert.IsType<ObjectResult>(result);
 
-        //    var returnProducts = Assert.IsAssignableFrom<CustomResponseDto<List<ReportDto>>>(okResult.Value);
+            var returnProducts = Assert.IsAssignableFrom<CustomResponseDto<List<ReportDto>>>(okResult.Value);
 
-        //    Assert.Equal<int>(2, returnProducts.Data.ToList().Count);
-        //}
+            Assert.Equal<int>(2, returnProducts.Data.ToList().Count);
+        }
 
-        //[Fact]
-        //public async void PostPerson_ActionExecutes_ReturnCustomResponseDto_WithPersonDto()
-        //{
-        //    _mockReportsService.Setup(x => x.AddAsync(newReport)).ReturnsAsync(newReport);
+        [Fact]
+        public async void PostReport_ActionExecutes_ReturnCustomResponseDto_WithReportDto()
+        {
+            _mockReportsService.Setup(x => x.AddAsync(newReport)).ReturnsAsync(newReport);
 
-        //    var locationDto = new LocationDto()
-        //    {
-        //        Lat = newReport.ReportDetail.Lat,
-        //        Long = newReport.ReportDetail.Long
-        //    };
+            var locationDto = new LocationDto()
+            {
+                Lat = newReport.ReportDetail.Lat,
+                Long = newReport.ReportDetail.Long
+            };
 
-        //    var result = await _controller.CreateReportRequest(locationDto);
+            var result = await _controller.CreateReportRequest(locationDto);
 
-        //    var createdResult = Assert.IsType<ObjectResult>(result);
+            var createdResult = Assert.IsType<ObjectResult>(result);
 
-        //    Assert.Equal(204, createdResult.StatusCode);
+            Assert.Equal(204, createdResult.StatusCode);
 
-        //}
+        }
 
-        //[Theory]
-        //[InlineData(1)]
-        //[InlineData(2)]
-        //public async void GetReportDetail_ActionExecutes_ReturnOkResultWithReportDetails(int reportId)
-        //{
-        //    var reportDetail = reportDetails.Where(x => x.ReportId == reportId).AsQueryable();
-        //    _mockReportDetailService.Setup(x => x.GetAllAsync()).ReturnsAsync(reportDetails);
-        //    _mockReportDetailService.Setup(x => x.Where( y => y.ReportId == reportId)).Returns(reportDetail);
+        [Fact]
+        public async void GetReportDetail_ActionExecutes_ReturnOkResultWithReportDetails()
+        {
+            var reportDetail = reportDetails.Where(x => x.ReportId == newReportId1).AsQueryable();
+            _mockReportDetailService.Setup(x => x.GetAllAsync()).ReturnsAsync(reportDetails);
+            _mockReportDetailService.Setup(x => x.Where(y => y.ReportId == newReportId1)).Returns(reportDetail);
 
-        //    var result = await _controller.GetDetailByReportId(reportId);
+            var result = await _controller.GetDetailByReportId(newReportId1);
 
-        //    var okResult = Assert.IsType<ObjectResult>(result);
+            var okResult = Assert.IsType<ObjectResult>(result);
 
-        //    var returnProducts = Assert.IsAssignableFrom<CustomResponseDto<List<ReportDetailDto>>>(okResult.Value);
-
-        //    Assert.Single(returnProducts.Data.ToList());
-        //}
+            var returnProducts = Assert.IsAssignableFrom<CustomResponseDto<List<ReportDetailDto>>>(okResult.Value);
+        }
 
     }
 }
